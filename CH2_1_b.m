446456632c534fdfe87f0ca2327afcb87decb1a9
chensonglu <chenslvs@163.com>
2015-12-20 18:29:20 +0800

    CH2_1

--- CH2_1_b.m
+++ CH2_1_b.m
@@ -8,7 +8,7 @@ function g = CH2_1_b(x, u, sigma, P)
 %   P     - Prior probability
 %
 % Ouputs:
-%  g      - Discriminant result 
+%   g     - Discriminant result 
 
 g = -0.5*(x - u)'*inv(sigma)*(x - u) - size(u, 1)/2.0*log(2*pi) - 0.5*log(det(sigma)) + log(P);
 end
